64d948e17cb23ef2ccaa0558ff8a84e3e047e487
Thales Luiz <thalesluizbraga@gmail.com>
2024-05-22 21:51:22 -0300

    feat: data types check, data types treatment, check na and treatment na added

--- eda_rossmann.py
+++ eda_rossmann.py
@@ -2,7 +2,6 @@
 # 1 - Libs Imports
 import datetime
 import inflection
-
 import pandas as pd
 
 
@@ -26,6 +25,7 @@ snake_case = lambda x: inflection.underscore(x)
 cols_new_names = list(map(snake_case,col_old_names))
 df.columns = cols_new_names
 
+
 # %%
 
 # 4 Data Description
@@ -37,7 +37,14 @@ print(f'The number of rows is: {df1.shape[0]}')
 print(f'The number of columns is: {df1.shape[1]}')
 
 
+# 4.2 Check Data Types
+df1.dtypes
 
+
+# 3.3 Change Data Types
+
+df1['date']  = pd.to_datetime(df['date'])
+df1['sales'] = df1['sales'].astype(float)
 
 # 4.3 Check NA
 df1.isna().sum()
@@ -46,9 +53,12 @@ df1.isna().sum()
 # %%
 
 # 4.4 NA Treatmet
+
 df1['competition_open_since_month'] = df1.apply(lambda x: x['date'].month
                                                 if pd.isna(x['competition_open_since_month'])
                                                 else x['competition_open_since_month'], axis=1 )
+
+#%%
 
 
 # %%
@@ -61,9 +71,7 @@ df1.dtypes
 # %%
 df1.head()
 # %%
-df1['date']  = pd.to_datetime(df['date'])
-df1['sales'] = df1['sales'].astype(float)
-df1['competition_open_since_year'] = df1['competition_open_since_year'].astype(int)
+
 
 # %%
 df1.dtypes
